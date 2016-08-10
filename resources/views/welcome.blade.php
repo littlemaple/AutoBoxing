8be37f962f07b9cd5ef814ad3a561f524c41e8fc
lintao <lintao@medzone-biotech.com>
2016-08-10 11:11:45 +0800

    update index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Android</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Auto Boxing</div>
+                <div class="title">Android Pregnancy</div>
             </div>
         </div>
     </body>
